413a811a0e296e1eab62c6617ba5653fae4470c0
andymill <andymill@gmail.com>
2015-12-21 18:39:09 -0800

    Initial Commit

---
+++ CoffeeFindr/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeFindr
+//
+//  Created by Andrew Miller on 12/21/15.
+//  Copyright (c) 2015 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
